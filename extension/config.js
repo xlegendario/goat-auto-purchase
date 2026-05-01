5277e78bd9801dc307d7bcb5e2127fe1c36a7091
xlegendario <dario.b1996@gmail.com>
2026-05-01 20:59:52 +0200

    Create config.js

---
+++ extension/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  BACKEND_URL: "https://goat-auto-purchase.onrender.com",
+  RUNNER_NAME: "test-store-auto-supply-runner",
+  ACCOUNT_GROUP_KEY: "main-account",
+  DRY_RUN: true
+};
